3faf69bccea0f79e371bd8afa34779b486177837
spish-web <nickolas.spisak@gmail.com>
2022-06-04 03:22:21 -0400

    progress

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void textcolor(int color);
   struct payroll
   {
+    string name;
     float rate_per_day, ss, healthcare;
     int total_days_work;
   };
@@ -17,8 +18,36 @@ float compute_net(float rate_per_day, float days_worked, float ss, float healthc
     float gross_pay = 0.0, deductions = 0.0, net_income = 0.0;
     gross_pay = rate_per_day * days_worked;
     deductions = ss + healthcare;
+    net_income = gross_pay + deductions;
+    return (net_income);
   }
-int main() {
 
-  return 0;
-}
+float compute_deductions(float ss,float healthcare)
+  {
+    return(ss + healthcare);
+  }
+
+int main() 
+  {
+    payroll employee;
+    cout<<"\t PAYROLL SYSTEM\n\n";
+    cout<<" Enter Employee Name     :";
+    getline(cin,employee.name);
+
+    cout<<" Enter Employees Rate per Day   :";
+    cin>>employee.rate_per_day;
+    cout<<" Enter Number of Days Worked   :";
+    cin>>employee.total_days_work;
+    cout<<"\n\n === DEDUCTIONS === ";
+    cout<<"\n\n\n SS Contribution    :";
+    cin>>employee.ss;
+    cout<<" Healthcare Contribution   :";
+    cin>>employee.healthcare;
+
+    //cout<<"\n\n"<< fixed << setprecision(2);
+    cout<<" Total Deductions => Php"
+        << compute_deductions(employee.ss, employee.healthcare);
+    cout<<"\n Monthly Gross Income    : Php "
+        << compute_gross(employee.rate_per_day, employee.total_days_work);
+    return 0;
+  }
