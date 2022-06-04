11fce9bb9f2c018cff4e73e01480cd709a1ce7a6
spish-web <nickolas.spisak@gmail.com>
2022-06-04 03:36:03 -0400

    done for now

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 # include <iostream>
+# include <iomanip>
 using namespace std;
 
 void textcolor(int color);
   struct payroll
   {
     string name;
-    float rate_per_day, ss, healthcare;
+    float rate_of_pay, ss, healthcare;
     int total_days_work;
   };
 
-float compute_gross(float rate_per_day, float days_worked)
+float compute_gross(float rate_of_pay, float days_worked)
   {
-    return(rate_per_day * days_worked);
+    return(rate_of_pay * days_worked);
   };
-float compute_net(float rate_per_day, float days_worked, float ss, float healthcare)
+float compute_net(float rate_of_pay, float days_worked, float ss, float healthcare)
   {
     float gross_pay = 0.0, deductions = 0.0, net_income = 0.0;
-    gross_pay = rate_per_day * days_worked;
+    gross_pay = rate_of_pay * days_worked;
     deductions = ss + healthcare;
     net_income = gross_pay + deductions;
     return (net_income);
@@ -34,8 +35,8 @@ int main()
     cout<<" Enter Employee Name     :";
     getline(cin,employee.name);
 
-    cout<<" Enter Employees Rate per Day   :";
-    cin>>employee.rate_per_day;
+    cout<<" Enter Employees Rate of Pay   :";
+    cin>>employee.rate_of_pay;
     cout<<" Enter Number of Days Worked   :";
     cin>>employee.total_days_work;
     cout<<"\n\n === DEDUCTIONS === ";
@@ -44,10 +45,12 @@ int main()
     cout<<" Healthcare Contribution   :";
     cin>>employee.healthcare;
 
-    //cout<<"\n\n"<< fixed << setprecision(2);
+    cout<<"\n\n"<< fixed << setprecision(2);
     cout<<" Total Deductions => Php"
         << compute_deductions(employee.ss, employee.healthcare);
     cout<<"\n Monthly Gross Income    : Php "
-        << compute_gross(employee.rate_per_day, employee.total_days_work);
+        << compute_gross(employee.rate_of_pay, employee.total_days_work)<<"\n\n";
+
+        system("pause");
     return 0;
   }
